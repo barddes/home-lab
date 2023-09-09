34c08250a101784d3067c42f32b1e191f2896477
Cesar Bueno <barddes@gmail.com>
2023-09-09 16:06:32 -0300

    Update arch-bootstrap.sh

--- scripts/arch-bootstrap.sh
+++ scripts/arch-bootstrap.sh
@@ -5,7 +5,7 @@ sed -i -E 's/#(pt_BR.UTF-8 UTF-8)/\1/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "jedric" > /etc/hostname
-pacman -Sy --noconfirm --needed grub efibootmgr os-prober ranger vim git curl wget iw iwd wireless_tools man-db man-pages texinfo dhcpcd amd-ucode lvm2
+pacman -Sy --noconfirm --needed grub efibootmgr os-prober ranger vim git curl wget iw iwd alacritty wireless_tools man-db man-pages texinfo dhcpcd amd-ucode lvm2
 sed -i 's/^\(HOOKS=.*\)\(block filesystems\)/\1block lvm2 filesystems/' /etc/mkinitcpio.conf
 mkinitcpio -P
 echo 'root:root' | chpasswd
