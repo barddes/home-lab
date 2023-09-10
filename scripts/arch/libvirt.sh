7c10dd9d37ed24bc44ecea7562089e18bb8cfa42
Cesar Bueno <barddes@gmail.com>
2023-09-10 16:13:07 -0300

    add history

--- scripts/arch/libvirt.sh
+++ scripts/arch/libvirt.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
 pacman -S --noconfirm --needed libvirt qemu-base iptables-nft 
+systemctl start libvirtd virtlogd
 systemctl enable libvirtd
+
+sed -i '/amd_iommu=/! s/^\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 amd_iommu=on"/' /etc/default/grub
+
+cat <<EOF > /etc/modprobe.d/kvm.conf
+options kvm ignore_msrs=1
+EOF
+
+cat <<EOF > /etc/modprobe.d/vfio.conf
+options vfio_iommu_type1 allow_unsafe_interrupts=1
+options vfio-pci ids=10de:2504,10de:228e
+EOF
+
+cat <<EOF > /etc/modprobe.d/blacklist.conf
+blacklist nvidia*
+blacklist nouveau
+EOF
+
+cat <<EOF >> /etc/mkinitcpio.conf
+MODULES=(vfio vfio_iommu_type1 vfio_pci ${MODULES[@]})
+EOF
+
+mkinitcpio -P
+grub-mkconfig -o /boot/grub/grub.cfg
