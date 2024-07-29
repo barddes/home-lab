8ac21d9991fe65d45084e5048af8dfc198d7c947
Cesar Bueno <barddes@gmail.com>
2024-07-29 04:39:06 -0300

    Clean

--- arch/scripts/i3wm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pacman -Sy --noconfirm xorg xf86-video-amdgpu xorg-xinit \
-	i3-wm lightdm dmenu lightdm-gtk-greeter alacritty \
-	firefox i3status
-
-echo "exec i3" > /root/.xinit
-echo "exec i3" | tee /home/cesar/.xinit || true
-systemctl enable lightdm
