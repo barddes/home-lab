8974e50bda82d30e23f2525b4f42df1d222347bb
Cesar Bueno <barddes@gmail.com>
2024-08-05 11:27:11 -0300

    update

--- k3s/configure-k3s.sh
+++ k3s/configure-k3s.sh
@@ -3,8 +3,9 @@
 set -e
 
 mkdir -p /etc/rancher/k3s
+internal_ip=$(ip addr | grep 192.168 | head -n 1 | sed -E 's|.+(192\.168\..+)/.+|\1|')
 cat <<EOF >/etc/rancher/k3s/config.yaml
 write-kubeconfig-mode: "0644"
 tls-san:
-  - "192.168.3.104"
+  - "$internal_ip"
 EOF
